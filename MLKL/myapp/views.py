396fc0834e1cb5efa7da3e9226302a36967ac8e9
bayashi (EDF) <116510574+saiyaman28@users.noreply.github.com>
2024-02-01 10:12:00 +0800

    Add files via upload

---
+++ MLKL/myapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from .models import TodoItem
+
+# Create your views here.
+def home(request):
+    # return HttpResponse("Hello World!")
+    return render(request, "home.html")
+
+def about(request):
+    return render(request, "about.html")
+
+def contact(request):
+    return render(request, "contact.html")
+
+def todos(request):
+    items = TodoItem.objects.all()
+    return render(request, "todos.html", {"todos": items})
